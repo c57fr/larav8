d062ccde56efc5d10acde088d8bc44676000314a
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-18 12:06:19 +0100

    Init Tuto POO Emacs

---
+++ poo/01_classes/Personnes.php
@@ -0,0 +1,31 @@
+<?php
+ 
+  class Personne
+  {
+    // Attributs
+    public $nom;
+    public $prenom;
+    public $dateDeNaissance;
+    public $taille;
+    public $sexe;
+ 
+    // Constantes
+    const NOMBRE_DE_BRAS = 2;
+    const NOMBRE_DE_JAMBES = 2;
+    const NOMBRE_DE_YEUX = 2;
+    const NOMBRE_DE_PIEDS = 2;
+    const NOMBRE_DE_MAINS = 2;
+ 
+    // Méthodes  
+    public function __construct() { }
+ 
+    public function boire()
+    {
+      echo 'La personne boit<br/>';
+    }
+ 
+    public function manger()
+    {
+      echo 'La personne mange<br/>';
+    }
+  }
