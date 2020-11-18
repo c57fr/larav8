4d47fb219c20a28a77e6833b376550d96ff49ead
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-18 12:21:27 +0100

    Up msg Auto cs-fixer

--- poo/01_classes/Personnes.php
+++ poo/01_classes/Personnes.php
@@ -1,29 +1,34 @@
 <?php
- 
+
+/*
+ * Ce fichier est la propriété de larav8 (c) 2020
+ */
+
   class Personne
   {
+    // Constantes
+    const NOMBRE_DE_BRAS   = 2;
+    const NOMBRE_DE_JAMBES = 2;
+    const NOMBRE_DE_YEUX   = 2;
+    const NOMBRE_DE_PIEDS  = 2;
+    const NOMBRE_DE_MAINS  = 2;
     // Attributs
     public $nom;
     public $prenom;
     public $dateDeNaissance;
     public $taille;
     public $sexe;
- 
-    // Constantes
-    const NOMBRE_DE_BRAS = 2;
-    const NOMBRE_DE_JAMBES = 2;
-    const NOMBRE_DE_YEUX = 2;
-    const NOMBRE_DE_PIEDS = 2;
-    const NOMBRE_DE_MAINS = 2;
- 
-    // Méthodes  
-    public function __construct() { }
- 
+
+    // Méthodes
+    public function __construct()
+    {
+    }
+
     public function boire()
     {
       echo 'La personne boit<br/>';
     }
- 
+
     public function manger()
     {
       echo 'La personne mange<br/>';
