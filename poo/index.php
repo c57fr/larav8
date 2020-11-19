7eb713aa4854b35e60200ed5fac171e02debf6f4
Momo <seckndanane16@gmail.com>
2020-11-20 00:48:24 +0100

    Accès à une constante

--- poo/index.php
+++ poo/index.php
@@ -8,9 +8,22 @@
 <body>
     <?php
     include_once 'cours/01_Instanciation.php';
+        // Affichage du nom et du prénom de la personne 1
+        echo 'Affichage du nom et du prénom de la personne 1<br/>';
         echo 'Personne1 <br/>';
         echo 'Nom: '.$personne1->nom.'<br/>';
         echo 'Prenom: ' .$personne1->prenom. '<br/>';
+
+        echo '-----------------------------------------------------------------------------------------<br/>';
+        
+        // Accès à une constante
+        echo 'Accès à une constante <br/>';
+        echo 'Chaque personne a ' .Personne::NOMBRE_DE_YEUX. ' yeux<br/>';
+        echo 'Chaque personne a ' .Personne::NOMBRE_DE_MAINS. ' mains<br/>';
+
+        echo '-----------------------------------------------------------------------------------------<br/>';
+
+
         
     ?>
 </body>
