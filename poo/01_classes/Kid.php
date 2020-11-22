bff0a477a1fc0ba850ef2d3314e5a63fa70961cf
Momo <seckndanane16@gmail.com>
2020-11-22 14:18:55 +0100

    Accès privé et protégé des propriétés

--- poo/01_classes/Kid.php
+++ poo/01_classes/Kid.php
@@ -1,11 +1,6 @@
 <?php
 class Kid{
     // Attributs
-    public $age;
-
-    // Methodes
-    public function showAge()
-    {
-        return 'Son age est : '. $this->age;
-    }
+    private $age;
+    
 }
--- poo/cours/03_controller.php
+++ poo/cours/03_controller.php
@@ -3,5 +3,5 @@ include_once './01_classes/Kid.php';
 echo str_repeat('-', 90).'<br>';
 echo 'Exo 3<br>';
 $billy = new Kid();
-$billy->age = 'rouge';
+$billy->age = "Ecore plus rouge qu'avant";
 echo $billy->showAge();
