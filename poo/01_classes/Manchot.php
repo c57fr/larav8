4eba0faa9c31105cd781eeb32fecb618bc3fa1d6
Lionel COTE <grcote7@gmail.com>
2020-11-24 16:20:32 +0100

    Reset pour 04

--- poo/01_classes/Manchot.php
+++ poo/01_classes/Manchot.php
@@ -1,10 +1,14 @@
 <?php
 
+/*
+ * Ce fichier est la propriété de larav8 (c) 2020
+ */
+
 class Manchot
 {
-    // Methodes
-    private function __call($method, $arguments)
-    {
-        echo 'Vous avez appele la methode '. $method. 'avec les arguments : ' . implode(', ', $arguments);
-    }
+  // Methodes
+  public function __call($method, $arguments)
+  {
+    echo 'Vous avez appele la methode '.$method.'avec les arguments : '.implode(', ', $arguments);
+  }
 }
--- poo/01_classes/SearchEngine.php
+++ poo/01_classes/SearchEngine.php
@@ -1,20 +1,22 @@
 <?php
 
+/*
+ * Ce fichier est la propriété de larav8 (c) 2020
+ */
+
 class SearchEngine
 {
-    public function search($conditions = array())
-    {
-        $query = 'SELECT id FROM table';
-        if(sizeof($conditions) > 0){
-            //$query.='WHERE '. implode('AND '.$conditions);
-            $query.=' WHERE '.implode(' AND ',$conditions);
-        }
-    
-
-        // Execution de la requete SQL avec une classe PDO
-        $result = SPDO::getInstance()->query($query);
-        //$result = SPDO::getInstance()->query($query);
-        $return = $result->fetchAll(PDO::FETCH_ASSOC);
-        return $return;
+  public function search($conditions = [])
+  {
+    $query = 'SELECT id FROM table';
+    if (count($conditions) > 0) {
+      //$query.='WHERE '. implode('AND '.$conditions);
+      $query .= ' WHERE '.implode(' AND ', $conditions);
     }
+
+    // Execution de la requete SQL avec une classe PDO
+    $result = PDO::query($query);
+    //$result = SPDO::getInstance()->query($query);
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
