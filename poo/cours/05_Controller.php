d0844b518f014c72fe9567a02fdc5afbb2b4a2d1
Momo <seckndanane16@gmail.com>
2020-11-25 00:24:43 +0100

    Le mot-clé clone

--- poo/cours/05_Controller.php
+++ poo/cours/05_Controller.php
@@ -8,3 +8,20 @@ $oNewDot = $oDot;
 $oNewDot->setCoords(20,20);
 echo '<br>';
 var_dump($oDot);
+echo '<br>';
+
+echo str_repeat('-', 90);
+
+echo '<br>';
+
+$oNewDot = clone $oDot;
+
+$oDot = new Point;
+$oDot->setCoords(10,10);
+var_dump($oDot);
+echo '<br>';
+$oNewDot = clone $oDot;
+$oNewDot->setCoords(20,20);
+var_dump($oDot);
+echo '<br>';
+var_dump($oNewDot);
