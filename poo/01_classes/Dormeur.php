ae94d6e85caf6d2dfa36964c945e7ab56fa20741
Momo <seckndanane16@gmail.com>
2020-11-25 01:40:24 +0100

    Méthodes magiques __sleep() et __wakeup()

--- poo/01_classes/Dormeur.php
+++ poo/01_classes/Dormeur.php
@@ -9,13 +9,24 @@ class Dormeur {
    
     public $_gouts;
    
-    /**
-     * Constructeur de la classe
-     */
-    public function __construct() {
+
+    public function __construct() 
+    {
       $this->_age = 19;
       $this->_aSonBonnet = true;
       $this->_couleurPreferee = 'rouge';
       $this->_gouts = array('musique','cinéma','curling');
     }
+
+    public function __sleep() 
+    {
+        echo 'Bon ben moi, je vais dormir.';
+        return array('_age','_aSonBonnet','_couleurPreferee');
+    }
+
+    public function __wakeup() 
+    {
+        echo 'bon ben moi, je vais me faire un café.';
+    }
+
   }
