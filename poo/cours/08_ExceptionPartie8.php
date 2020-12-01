f4ba7359376a4181edc8ac05868802e7d0c0ac44
Momo <seckndanane16@gmail.com>
2020-12-01 23:38:30 +0100

    Exemple d'interception d'une exception

--- poo/cours/08_ExceptionPartie8.php
+++ poo/cours/08_ExceptionPartie8.php
@@ -7,9 +7,24 @@
 //$e = new Exception("Une errer s4est produite");
 
 
-$password = 'Toto';
+/*$password = 'Toto';
   if('Emacs' !== $password) {
     throw new Exception('Votre password est incorrect !');
   }
 
-  echo 'Bonjour Emacs';
+  echo 'Bonjour Emacs';*/
+
+  try{
+    $password = 'Toto';
+
+    if('Emacs' !== $password){
+      throw New Exception('Votre password est incorrect !');
+    }
+
+    echo 'Bonjour Emacs';
+  }
+  catch(Exception $e)
+  {
+    echo "L'erreur suivante a été générée : ". "\n";
+    echo $e->getMessage();
+  }
