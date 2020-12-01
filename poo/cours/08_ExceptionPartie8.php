e40258f89e01070d5c00ecee45c8abf3811a5e1c
Momo <seckndanane16@gmail.com>
2020-12-01 23:28:31 +0100

    Exemple de lancement d'une Exception à travers le programme

--- poo/cours/08_ExceptionPartie8.php
+++ poo/cours/08_ExceptionPartie8.php
@@ -1,18 +1,15 @@
 <?php
 
 
-include_once './classes/Exception.php';
+//include_once './classes/Exception.php';
 
 
-$e = new Exception("Une errer s4est produite");
+//$e = new Exception("Une errer s4est produite");
 
 
 $password = 'Toto';
   if('Emacs' !== $password) {
     throw new Exception('Votre password est incorrect !');
   }
- 
-  // Cette ligne ne sera jamais exécutée
-  // car une exception est lancée pour interrompre
-  // l'exécution normale du programme
+
   echo 'Bonjour Emacs';
