3765229d73960bfa69b4b56b34a51688881d59b2
Momo <seckndanane16@gmail.com>
2020-12-03 00:23:10 +0100

    last exo iterator

--- poo/classes/MaClasse.php
+++ poo/classes/MaClasse.php
@@ -1,6 +1,14 @@
 <?php
+/*interface Iterator
+{
+    public function rewind();
+    public function key();
+    public function current();
+    public function next();
+    public function valid();
+}*/
 
-class MaClasse
+/*class MaClasse
 {
     protected $arg1 = 'plop';
     protected $arg2 = 'plip';
@@ -11,4 +19,42 @@ $c = new Maclasse();
 foreach ($c as $key=>$value)
 {
     echo $key .' : '. $value. '<br>';
+}
+*/
+
+class Maclasse implements Iterator 
+{
+    protected $n;
+    const MAX = 5;
+
+    public function rewind()
+    {
+        $this->n = 0;
+    }
+
+    public function next()
+    {
+        $this->n++;
+    }
+
+    public function key()
+    {
+        return 'increment '.($this->n+1);
+    }
+
+    public function current()
+    {
+        return $this->n;
+    }
+
+    public function valid()
+    {
+        return $this->n<=self::MAX;
+    }
+}
+
+$c = new Maclasse();
+foreach($c as $key => $val)
+{
+    echo $key.' : '.$val. '<br>';
 }
