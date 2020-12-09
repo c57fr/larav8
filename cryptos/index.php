5069f7a1fccdaa33d35c0feddcf6b0b2d014d92e
Lionel COTE <grcote7@gmail.com>
2020-12-09 11:57:39 +0100

    Optimisation boucle tokens

--- cryptos/index.php
+++ cryptos/index.php
@@ -4,48 +4,44 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Cryptos</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-  <div class="container flex">
+<div class="container flex">
+<?php
+$i      = 0;
+$tokens = ['ALGOUSD', 'FILUSD', 'KSMUSD', 'ZECUSD'];
+foreach ($tokens as $tk) {
+  ++$i;
+  $tk = 'KRAKEN:'.$tk; ?>
+<div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <script type="text/javascript"
+      src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+      {
+        "interval": "1m",
+        "width": "720",
+        "isTransparent": false,
+        "height": "435",
+        "symbol": "<?php echo $tk; ?>",
+        "showIntervalTabs": true,
+        "locale": "fr",
+        "colorTheme": "light"
+      }
+    </script>
+  </div>
+  <?php
+  if (0 === $i % 3) {
+    echo "</div>
+  <div class='container flex'>";
+  }
+}
 
-    <!-- TradingView Widget BEGIN -->
-  <div class="tradingview-widget-container">
-    <div class="tradingview-widget-container__widget"></div>
-    <script type="text/javascript"
-      src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
-      {
-        "interval": "1m",
-        "width": "720",
-        "isTransparent": false,
-        "height": "435",
-        "symbol": "KRAKEN:ALGOUSD",
-        "showIntervalTabs": true,
-        "locale": "fr",
-        "colorTheme": "light"
-      }
-    </script>
-  </div>
-  <div class="tradingview-widget-container">
-    <div class="tradingview-widget-container__widget"></div>
-    <script type="text/javascript"
-      src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
-      {
-        "interval": "1m",
-        "width": "720",
-        "isTransparent": false,
-        "height": "435",
-        "symbol": "KRAKEN:FILUSD",
-        "showIntervalTabs": true,
-        "locale": "fr",
-        "colorTheme": "light"
-      }
-    </script>
-  </div>
+?>
 </div>
-  <!-- TradingView Widget END -->
+ 
   <footer>
     <div class="tradingview-widget-copyright"><a href="https://fr.tradingview.com/symbols/ALGOUSD/technicals/"
     rel="noopener" target="_blank"><span class="blue-text"> Analyse technique pour ALGOUSD</span></a> par
