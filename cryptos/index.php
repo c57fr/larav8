d90368eb02bb5ff0d7c3566283b5fa5fb6584167
Lionel COTE <grcote7@gmail.com>
2020-12-23 17:56:17 +0100

    Up cryptos

--- cryptos/index.php
+++ cryptos/index.php
@@ -15,10 +15,31 @@ $i      = 0;
 $tokens = ['ALGOUSD', 'BTCUSD', 'COMPUSD', 'XDGUSD', 'DOTUSD', 'FILUSD', 'GNOUSD', 'KSMUSD', 'ZECUSD'];
 foreach ($tokens as $tk) {
   ++$i;
-  $tk = 'KRAKEN:'.$tk;
+  $tk = 'KRAKEN:'.$tk; ?>
+<div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <script type="text/javascript"
+      src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+      {
+        "interval": "1m",
+        "width": "720",
+        "isTransparent": false,
+        "height": "435",
+        "symbol": "<?php echo $tk; ?>",
+        "showIntervalTabs": true,
+        "locale": "fr",
+        "colorTheme": "light"
+      }
+    </script>
+  </div>
+  <?php
+  if (0 === $i % 3) {
+    echo "</div>
+    <div class='container flex'>";
+  }
 }
-  ?>
-
+?>
+</div>
 
  
   <footer>
