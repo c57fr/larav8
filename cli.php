e787849c63e8f462a69ed6539d69f1c6b85f6237
Lionel COTE <grcote7@gmail.com>
2021-01-19 00:11:27 +0100

    Saisie en CLI

---
+++ cli.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Ce fichier est la propriété de larav8 (c) 2020
+ */
+
+echo 'Attention: Code pour fonctionner uniquement en CLI<hr>
+Give me a Number ?';
+exit; // Commenter pour faire tourner en CLI
+$nbr = readline('Votre réponse ? ');
+echo 'Vous avez saisi: '.$nbr;
